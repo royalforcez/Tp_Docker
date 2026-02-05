51fb1a1a747a1b9962ec2060fbe40c73cb6600ac
CazinSacha <cazin.lep@gmail.com>
2026-02-05 16:06:19 +0100

    delete href test

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = docuement.getElementById("btn2");
 
-btn1.addEventListener("click", function () {
-    window.location.href = "index2.html";
-});
-
-btn2.addEventListener("click", function () {
-    window.location.href = "index3.html";
-});
