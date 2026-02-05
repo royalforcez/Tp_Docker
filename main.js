7cc8a7429e0bd09aad2e872de9b670bf1f684527
CazinSacha <cazin.lep@gmail.com>
2026-02-05 15:59:06 +0100

    2 pages + main js

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = docuement.getElementById("btn2");
+
+btn1.addEventListener("click", function () {
+    window.location.href = "index2.html";
+});
+
+btn2.addEventListener("click", function () {
+    window.location.href = "index3.html";
+});
